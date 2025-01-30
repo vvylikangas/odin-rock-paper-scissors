0be6bb3da4f72a5d87f5a4e7e98d82bbdcb884ff
Ville Ylikangas <villeylik@gmail.com>
2025-01-30 12:58:24 +0200

    Add logic for single round

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const rpsArr = ['rock', 'paper', 'scissors'];
   const rndIdx = Math.floor(Math.random() * rpsArr.length);
   return rpsArr[rndIdx];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
   let humanChoice = prompt('Type "rock", "paper" or "scissors": ');
   return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  let human = humanChoice.toLowerCase();
+  let computer = computerChoice;
+
+  if (
+    (human === 'paper' && computer === 'scissors') ||
+    (human === 'scissors' && computer === 'rock') ||
+    (human === 'rock' && computer === 'paper')
+  ) {
+    console.log(`You lose! ${computer} beats ${human}`);
+    computerScore += 1;
+  } else if (human === computer) {
+    console.log("It's a draw!");
+  } else {
+    console.log(`You win! ${human} beats ${computer}`);
+    humanScore += 1;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
